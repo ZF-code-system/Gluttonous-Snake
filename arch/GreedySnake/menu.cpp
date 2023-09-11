0539451b8e89144f9bbbe0ee81ffd21c5168b36b
NIUTR <1958577075@qq.com>
2023-09-11 22:44:56 +0800

    完成边框的打印

--- arch/GreedySnake/menu.cpp
+++ arch/GreedySnake/menu.cpp
@@ -1,43 +1,47 @@
 #include "menu.h"
 #include <stdio.h>
+#include<conio.h>
+#include<Windows.h>
+
 
 /*
 	菜单中所需的数据
 */
 
 const int H = 30;         //游戏窗口的宽
-const int L = 120;           //游戏窗口的长
+const int L = 70;           //游戏窗口的长
 const char Border = '#';           //边界的样式
 char map_Border[H][L];             //存储地图边界
 
 
-/*
-	初始化边框
-*/
-static void initBorder()
-{
-	for (int i = 0; i < H; ++i) {
-		for (int j = 0; j < L; ++j) {
-			if (i == 0 || i == H - 1 || j == 0 || j == L - 1) {
-				map_Border[i][j] = Border;
-			}
-			else {
-				map_Border[i][j] = ' ';
-			}
-		}
-	}
-}
+
 
 /*
 	打印一个边框
 */
 void printBorder()
 {
-	initBorder();
-	for (int i = 0; i < H; ++i) {
-		for (int j = 0; j < L; ++j) {
-			printf("%c",map_Border[i][j]);
-		}
-		printf("\n");
+	gotoXY(25, 1);
+	for (int i = 0; i < L; ++i) putchar(Border);
+	gotoXY(25, H);
+	for (int i = 0; i < L; ++i)putchar(Border);
+	gotoXY(25, 2);
+	for (int i = 0; i < H-2; ++i) {
+		putchar(Border);
+		for (int i = 0; i < L - 2; ++i)putchar(' ');
+		putchar(Border);
+		gotoXY(25, 3 + i);
 	}
+	gotoXY(26, 2);
+}
+
+/*
+	将光标传递到制定位置
+*/
+void gotoXY(int x,int y)
+{
+	COORD c;
+	c.X = x - 1;
+	c.Y = y - 1;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
--- arch/GreedySnake/menu.h
+++ arch/GreedySnake/menu.h
@@ -5,3 +5,5 @@
 */
 
 void printBorder();        //打印游戏边界
+
+void gotoXY(int x,int y);     //将光标x,y传到制定位置
